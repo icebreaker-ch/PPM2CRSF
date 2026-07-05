cc1b9633fe1a956f54cf5690353b3d40d0649b44
Roland Schäuble <roland.schaeuble@gmail.com>
2026-07-05 16:39:10 +0200

    Save working copy

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,7 @@
 #define CRSF_BAUD 420000
 #define INVERT_SIGNAL true // true for output to a JR-Module, false wehen using an RX as TX
 
-volatile uint32_t lastTime = 0;
-volatile uint16_t ppmValues[NUM_CRSF_CHANNELS];
+volatile uint16_t ppmValues[NUM_PPM_CHANNELS];
 
 const uint8_t CRSF_SYNC_POS = 0;
 const uint8_t CRSF_LEN_POS = 1;
@@ -24,55 +23,50 @@ const uint8_t CRSF_TYPE = 0x16; // CRSF Frametype RC channels packed
 
 const uint16_t CRSF_MIN_US = 988;
 const uint16_t CRSF_MAX_US = 2012;
-const uint16_t CRSF_DIFF_US = CRSF_MAX_US - CRSF_MIN_US;
 const uint16_t CRSF_MIN_VALUE = 172;
 const uint16_t CRSF_MAX_VALUE = 1811;
-const uint16_t CRSF_DIFF_VALUE = CRSF_MAX_VALUE - CRSF_MIN_VALUE;
+const uint16_t CRSF_NEUTRAL = 992;
+const uint16_t CRSF_REFRESH_RATE = 10; // ms
 
+const uint16_t PPM_SYNC = 3000; // us
 const uint16_t PPM_NEUTRAL = 1500;
 
-const uint8_t REFRESH_RATE = 70; // Hz
+static uint8_t crsfFrame[CRSF_FRAME_SIZE];
 
-uint8_t crsfFrame[CRSF_FRAME_SIZE];
-
-void ppmISR()
-{
+void ppmISR() {
+    static long lastPPMPulse = 0;
     static int currentChannel = 0;
 
     uint32_t now = micros();
-    uint32_t duration = now - lastTime;
-    lastTime = now;
+    uint32_t duration = now - lastPPMPulse;
+    lastPPMPulse = now;
 
-    if (duration > 4000)
-    {
+    if (duration > PPM_SYNC) {
         currentChannel = 0;
-    }
-    else
-    {
-        if (currentChannel < NUM_PPM_CHANNELS)
-        {
+    } else {
+        if (currentChannel < NUM_PPM_CHANNELS) {
             ppmValues[currentChannel] = duration;
             currentChannel++;
         }
     }
 }
 
-void encodeChannels()
-{
-  uint16_t channels[NUM_CRSF_CHANNELS];
-  for (int i = 0; i < NUM_CRSF_CHANNELS; i++) {
-      uint16_t val = CRSF_MIN_VALUE + (ppmValues[i] - CRSF_MIN_US) * CRSF_DIFF_VALUE / CRSF_DIFF_US;
-      val = min(val, CRSF_MAX_VALUE);
-      val = max(val, CRSF_MIN_VALUE);
-      channels[i] = val;
-  }
+void encodeChannels() {
+    uint16_t channels[NUM_CRSF_CHANNELS];
+    for (int i = 0; i < NUM_CRSF_CHANNELS; i++) {
+        uint16_t val;
+        if (i < NUM_PPM_CHANNELS)
+            val = map(ppmValues[i], CRSF_MIN_US, CRSF_MAX_US, CRSF_MIN_VALUE, CRSF_MAX_VALUE);
+        else
+            val = CRSF_NEUTRAL;
+        channels[i] = max(min(val, CRSF_MAX_VALUE), CRSF_MIN_VALUE); // limit crsf min..crsf max
+    }
 
     // clear payload
     memset(&crsfFrame[CRSF_PAYLOAD_POS], 0, CRSF_PAYLOAD_SIZE);
 
     int bitOffset = 0;
-    for (int i = 0; i < NUM_CRSF_CHANNELS; i++)
-    {
+    for (int i = 0; i < NUM_CRSF_CHANNELS; i++) {
         uint16_t chan = channels[i] & 0x07FF; // 11 bits
         int byteIndex = bitOffset / 8;
         int bitIndex = bitOffset % 8;
@@ -80,22 +74,18 @@ void encodeChannels()
         crsfFrame[CRSF_PAYLOAD_POS + byteIndex] |= (chan << bitIndex) & 0xFF;
         crsfFrame[CRSF_PAYLOAD_POS + byteIndex + 1] |= (chan >> (8 - bitIndex)) & 0xFF;
 
-        if (bitIndex > 5)
-        {
+        if (bitIndex > 5) {
             crsfFrame[CRSF_PAYLOAD_POS + byteIndex + 2] |= (chan >> (16 - bitIndex)) & 0xFF;
         }
         bitOffset += 11;
     }
 }
 
-uint8_t crc8(const uint8_t *data, uint8_t length)
-{
+uint8_t crc8(const uint8_t *data, uint8_t length) {
     uint8_t crc = 0;
-    for (uint8_t i = 0; i < length; i++)
-    {
+    for (uint8_t i = 0; i < length; i++) {
         crc ^= data[i];
-        for (uint8_t j = 0; j < 8; j++)
-        {
+        for (uint8_t j = 0; j < 8; j++) {
             if (crc & 0x80)
                 crc = (crc << 1) ^ 0xD5;
             else
@@ -105,8 +95,7 @@ uint8_t crc8(const uint8_t *data, uint8_t length)
     return crc;
 }
 
-void sendCRSFFrame()
-{
+void sendCRSFFrame() {
     crsfFrame[CRSF_SYNC_POS] = CRSF_SYNC;
     crsfFrame[CRSF_LEN_POS] = CRSF_PAYLOAD_SIZE + 2;
     crsfFrame[CRSF_TYPE_POS] = CRSF_TYPE;
@@ -115,31 +104,25 @@ void sendCRSFFrame()
     CRSF_SERIAL.write(crsfFrame, CRSF_FRAME_SIZE);
 }
 
-
-void setup()
-{
-    for (int i = 0; i < NUM_CRSF_CHANNELS; ++i)
-    {
+void setup() {
+    for (int i = 0; i < NUM_PPM_CHANNELS; ++i) {
         ppmValues[i] = PPM_NEUTRAL;
     }
 
-    #ifdef RX
+#ifdef ESP32
     CRSF_SERIAL.begin(CRSF_BAUD, SERIAL_8N1, RX, TX, INVERT_SIGNAL);
-    #else
+#else
     CRSF_SERIAL.begin(CRSF_BAUD);
-    #endif
+#endif
     pinMode(PPM_PIN, INPUT);
     attachInterrupt(digitalPinToInterrupt(PPM_PIN), ppmISR, RISING);
 }
 
-void loop()
-{
-    static unsigned long lastSend = 0;
-
-    unsigned long now = millis();
-    if (now - lastSend > 1000 / REFRESH_RATE)
-    {
-        sendCRSFFrame();
+void loop() {
+    static long lastSend = 0;
+    long now = millis();
+    if (now - lastSend > CRSF_REFRESH_RATE) {
         lastSend = now;
+        sendCRSFFrame();
     }
 }
